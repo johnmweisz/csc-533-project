daeaf02d39401ed75025b95cffdc224d6937868a
John Weisz <johnmweisz@gmail.com>
2024-11-02 12:02:01 -0400

    improve

--- data_labeler.py
+++ data_labeler.py
@@ -1,13 +1,18 @@
 import pandas as pd
+from text_moderator import predict_pii
 
 file_path = 'data/labeled_data.csv'
 df = pd.read_csv(file_path)
 
 for index, row in df.iterrows():
+    text = row['tweet']
     if row['pii_class'] != 0:
         continue
+    if not predict_pii(text)[0]:
+        df.at[index, 'pii_class'] = 1
+        continue
     
-    print(row['tweet'])
+    print(f"{row['id']} - {text}")
     pii_response = input("Does this tweet contain PII? (y/n): ").strip().lower()
     
     if pii_response == 'y':
@@ -15,7 +20,6 @@ for index, row in df.iterrows():
     elif pii_response == 'n':
         df.at[index, 'pii_class'] = 1
     else:
-        print("Invalid input. Skipping this tweet.")
         continue
 
     df.to_csv(file_path, index=False)

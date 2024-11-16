608cfb2821ac027b5523c9c162190831c73afeb3
John Weisz <johnmweisz@gmail.com>
2024-11-16 18:27:00 -0500

    final label

--- data_labeler.py
+++ data_labeler.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from text_moderator import predict_pii, preprocess_text
 
 file_path = 'data/labeled_data.csv'
 df = pd.read_csv(file_path)
@@ -7,9 +6,6 @@ df = pd.read_csv(file_path)
 for index, row in df.iterrows():
     text = row['tweet']
     if row['pii_class'] != 0:
-        continue
-    if not predict_pii(preprocess_text(text))[0]:
-        df.at[index, 'pii_class'] = 1
         continue
     
     print(f"{row['id']} - {text}")
